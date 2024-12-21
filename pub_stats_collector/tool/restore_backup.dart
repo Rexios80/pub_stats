6ba1ee47176142d0971533d81144e0d129f3a64c
Rexios <rexios@rexios.dev>
2024-12-21 18:09:38 -0500

    Improving the import tool

--- pub_stats_collector/tool/restore_backup.dart
+++ pub_stats_collector/tool/restore_backup.dart
@@ -1,6 +1,10 @@
+// Too annoying
+// ignore_for_file: unawaited_futures
+
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:flutter_tools_task_queue/flutter_tools_task_queue.dart';
 import 'package:pub_stats_collector/credential/credentials.dart';
 import 'package:pub_stats_collector/service/firebase_service.dart';
 
@@ -13,32 +17,34 @@ void main() async {
 
   print('Restoring backup...');
 
+  Future<void> restoreForPackages(String path) async {
+    final queue = TaskQueue();
+    final data = backupData[path] as Map<String, dynamic>;
+    var i = 0;
+    for (final package in data.keys) {
+      queue.add(() async {
+        await database.ref('$path/$package').set(data[package]);
+        i++;
+        if (i % 100 == 0) {
+          print('Restored $path for $i/${data.length} packages');
+        }
+      });
+    }
+    await queue.tasksComplete;
+    print('Restored $path');
+  }
+
   // TODO: Restore alert configs?
 
-  final data = backupData['data'] as Map<String, dynamic>;
-  for (final package in data.keys) {
-    await database.ref('data/$package').set(data[package]);
-    print('Restored data for $package');
-  }
-  print('Restored data');
-
-  final diffs = backupData['diffs'] as Map<String, dynamic>;
-  for (final package in diffs.keys) {
-    await database.ref('diffs/$package').set(diffs[package]);
-    print('Restored diffs for $package');
-  }
-  print('Restored diffs');
+  await restoreForPackages('data');
+  await restoreForPackages('diffs');
 
   await database.ref('global_stats').set(backupData['global_stats']);
   print('Restored global_stats');
   await database.ref('package_counts').set(backupData['package_counts']);
   print('Restored package_counts');
 
-  final stats = backupData['stats'] as Map<String, dynamic>;
-  for (final package in stats.keys) {
-    await database.ref('stats/$package').set(stats[package]);
-    print('Restored stats for $package');
-  }
+  await restoreForPackages('stats');
 
   print('Restoration complete!');
   exit(0);

b312758019a35a19b6e018300cefe7b20de0baa3
Rexios <rexios@rexios.dev>
2025-01-21 22:31:11 -0500

    They definitely work

--- functions/src/index.dart
+++ functions/src/index.dart
@@ -4,9 +4,12 @@ import 'dart:js_interop_unsafe';
 import 'package:firebase_js_interop/admin.dart';
 import 'package:firebase_js_interop/admin/app.dart';
 import 'package:firebase_js_interop/express.dart' as express;
+import 'package:firebase_js_interop/extensions.dart';
 import 'package:firebase_js_interop/functions.dart';
 import 'package:firebase_js_interop/functions/https.dart';
+import 'package:firebase_js_interop/js.dart';
 import 'package:firebase_js_interop/node.dart';
+import 'package:pub_stats_core/pub_stats_core.dart';
 
 import 'badge_maker.dart';
 
@@ -15,7 +18,13 @@ final packageBadgeRegex = RegExp(r'^\/packages\/([^\/]+)\/([^\/]+)\.svg$');
 enum BadgeType {
   popularity,
   rank,
-  dependents,
+  dependents;
+
+  String get databaseKey => switch (this) {
+        popularity => 'ps2',
+        rank => 'n',
+        dependents => 'nd',
+      };
 }
 
 void main() {
@@ -32,8 +41,29 @@ void main() {
         return response.status(404).send(null);
       }
 
-      return response
-          .send(badgeMaker.makeBadge(BadgeFormat(message: 'test')).toJS);
+      final key = badgeType.databaseKey;
+      return promise(() async {
+        final snap =
+            await database.ref('data/$packageName/$key'.toJS).get().toDart;
+
+        final data = snap.val();
+        if (data == null) return response.status(404).send(null);
+
+        return response.send(
+          badgeMaker
+              .makeBadge(
+                BadgeFormat(
+                  label: badgeType.name,
+                  message: data.toString(),
+                  color: '#007ec6',
+                  links: [
+                    'https://pubstats.dev/packages/$packageName'.toJS,
+                  ].toJS,
+                ),
+              )
+              .toJS,
+        );
+      });
     }.toJS,
   );
 }

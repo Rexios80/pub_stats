cc5190024364a1fe990f4f3010446efd7b707770
Rexios <rexios@rexios.dev>
2025-01-22 19:38:02 -0500

    Increase CPUs

--- functions/src/index.dart
+++ functions/src/index.dart
@@ -35,7 +35,11 @@ void main() {
   final database = FirebaseAdmin.database.getDatabase();
 
   exports['fetchPackageData'] = FirebaseFunctions.https.onRequest(
-    HttpsOptions(timeoutSeconds: 600.toJS, memory: '2GiB'.toJS),
+    HttpsOptions(
+      timeoutSeconds: 600.toJS,
+      memory: '2GiB'.toJS,
+      cpu: '4000m'.toJS,
+    ),
     (Request request, express.Response response) {
       return promise(() => fetchPackageData(response));
     }.toJS,

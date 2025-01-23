f86b181851ad965ff28eb11bb1c065a4dbb920b1
Rexios <rexios@rexios.dev>
2025-01-23 14:49:51 -0500

    Add debug flag for building

--- functions/tool/build.dart
+++ functions/tool/build.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_js_interop/build.dart';
 
-void main() async {
-  await buildCloudFunctions();
+void main(List<String> args) async {
+  final debug = args.contains('--debug');
+  await buildCloudFunctions(
+    optimization: debug ? OptimizationLevel.O0 : OptimizationLevel.O2,
+  );
 }

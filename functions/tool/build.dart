ff693555f882085dc4fc4392aab6a70154bb1cb0
Rexios <rexios@rexios.dev>
2025-02-20 20:45:28 -0500

    Fix JS issue

--- functions/tool/build.dart
+++ functions/tool/build.dart
@@ -3,6 +3,6 @@ import 'package:firebase_js_interop/build.dart';
 void main(List<String> args) async {
   final debug = args.contains('--debug');
   await buildCloudFunctions(
-    optimization: debug ? OptimizationLevel.O0 : OptimizationLevel.O2,
+    optimization: debug ? OptimizationLevel.O1 : OptimizationLevel.O2,
   );
 }

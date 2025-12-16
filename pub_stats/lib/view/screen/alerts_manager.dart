1e05d19d7fbfd0a37f3c2e88b7a6f31b6331263d
Rexios <rexios@rexios.dev>
2025-12-16 03:05:16 -0500

    UI improvement

--- pub_stats/lib/view/screen/alerts_manager.dart
+++ pub_stats/lib/view/screen/alerts_manager.dart
@@ -20,171 +20,172 @@ class AlertsManager extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('Alerts Manager')),
       body: Center(
-        child: Column(
-          children: [
-            Container(
-              constraints: const BoxConstraints(maxWidth: 800),
-              padding: const EdgeInsets.all(16),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: SearchAnchor.bar(
-                      viewConstraints: const BoxConstraints(),
-                      viewBarPadding: const EdgeInsets.symmetric(
-                        horizontal: 16,
-                      ),
-                      shrinkWrap: true,
-                      barLeading: const SizedBox.shrink(),
-                      viewLeading: const SizedBox.shrink(),
-                      viewTrailing: const [],
-                      suggestionsBuilder: (context, controller) => const [
-                        ListTile(title: Text('".system" for system alerts')),
-                        ListTile(
-                          title: Text(
-                            '"packageName" for single package alerts',
-                          ),
+        child: FastBuilder(
+          () => Column(
+            children: [
+              Container(
+                constraints: const BoxConstraints(maxWidth: 800),
+                padding: const EdgeInsets.all(16),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: SearchAnchor.bar(
+                        viewConstraints: const BoxConstraints(),
+                        viewBarPadding: const EdgeInsets.symmetric(
+                          horizontal: 16,
                         ),
-                        ListTile(
-                          title: Text(
-                            '"publisher:publisherName" for publisher package alerts',
-                          ),
-                        ),
-                      ],
-                      searchController: slugController,
-                      onSubmitted: slugController.closeView,
-                      barHintText: 'Alert slug',
-                    ),
-                  ),
-                  const SizedBox(width: 16),
-                  SizedBox(
-                    width: 150,
-                    child: FastBuilder(
-                      () => DropdownButtonFormField(
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                        ),
-                        initialValue: selectedType.value,
-                        items: AlertConfigType.values
-                            .map(
-                              (e) => DropdownMenuItem(
-                                value: e,
-                                child: Text(e.name.titleCase),
-                              ),
-                            )
-                            .toList(),
-                        onChanged: (value) {
-                          if (value == null) return;
-                          selectedType.value = value;
-                        },
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 16),
-                  Expanded(
-                    child: FastBuilder(
-                      () => TextField(
-                        controller: extraController,
-                        decoration: InputDecoration(
-                          border: const OutlineInputBorder(),
-                          hintText: selectedType.value.extraLabel,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 16),
-                  PopupMenuButton(
-                    tooltip: 'Alert fields',
-                    itemBuilder: (context) => PackageDataField.values
-                        .map(
-                          (e) => PopupMenuItem(
-                            child: FastBuilder(
-                              () => CheckboxListTile(
-                                value: !ignoredFields.contains(e),
-                                onChanged: (value) {
-                                  if (value == true) {
-                                    ignoredFields.remove(e);
-                                  } else {
-                                    ignoredFields.add(e);
-                                  }
-                                },
-                                title: Text(e.name.titleCase),
-                              ),
+                        shrinkWrap: true,
+                        barLeading: const SizedBox.shrink(),
+                        viewLeading: const SizedBox.shrink(),
+                        viewTrailing: const [],
+                        suggestionsBuilder: (context, controller) => const [
+                          ListTile(title: Text('".system" for system alerts')),
+                          ListTile(
+                            title: Text(
+                              '"packageName" for single package alerts',
                             ),
                           ),
-                        )
-                        .toList(),
-                    child: FastBuilder(
-                      () => AlertFieldsButton(
-                        ignoredFields: ignoredFields..register(),
+                          ListTile(
+                            title: Text(
+                              '"publisher:publisherName" for publisher package alerts',
+                            ),
+                          ),
+                        ],
+                        searchController: slugController,
+                        onSubmitted: slugController.closeView,
+                        barHintText: 'Alert slug',
                       ),
                     ),
-                  ),
-                  IconButton(
-                    tooltip: 'Add alert',
-                    icon: const Icon(Icons.add),
-                    onPressed: addConfig,
-                  ),
-                ],
-              ),
-            ),
-            FastBuilder(() {
-              if (selectedType.value == AlertConfigType.telegram) {
-                return const Text('Add the pubstats_bot user to your chat');
-              } else {
-                return const SizedBox.shrink();
-              }
-            }),
-            Expanded(
-              child: SingleChildScrollView(
-                child: FastBuilder(
-                  () => DataTable(
-                    columns: const [
-                      DataColumn(label: Text('Slug')),
-                      DataColumn(label: Text('Type')),
-                      DataColumn(label: Text('Extra')),
-                      DataColumn(label: Text('Fields')),
-                      DataColumn(label: Text('Actions')),
-                    ],
-                    rows: _user.configs
-                        .map(
-                          (e) => DataRow(
-                            cells: [
-                              DataCell(Text(e.slug)),
-                              DataCell(Text(e.type.name.titleCase)),
-                              DataCell(
-                                SizedBox(width: 200, child: Text(e.extra)),
+                    const SizedBox(width: 16),
+                    SizedBox(
+                      width: 150,
+                      child: FastBuilder(
+                        () => DropdownButtonFormField(
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                          ),
+                          initialValue: selectedType.value,
+                          items: AlertConfigType.values
+                              .map(
+                                (e) => DropdownMenuItem(
+                                  value: e,
+                                  child: Text(e.name.titleCase),
+                                ),
+                              )
+                              .toList(),
+                          onChanged: (value) {
+                            if (value == null) return;
+                            selectedType.value = value;
+                          },
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 16),
+                    Expanded(
+                      child: FastBuilder(
+                        () => TextField(
+                          controller: extraController,
+                          decoration: InputDecoration(
+                            border: const OutlineInputBorder(),
+                            hintText: selectedType.value.extraLabel,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 16),
+                    PopupMenuButton(
+                      tooltip: 'Alert fields',
+                      itemBuilder: (context) => PackageDataField.values
+                          .map(
+                            (e) => PopupMenuItem(
+                              child: FastBuilder(
+                                () => CheckboxListTile(
+                                  value: !ignoredFields.contains(e),
+                                  onChanged: (value) {
+                                    if (value == true) {
+                                      ignoredFields.remove(e);
+                                    } else {
+                                      ignoredFields.add(e);
+                                    }
+                                  },
+                                  title: Text(e.name.titleCase),
+                                ),
                               ),
-                              DataCell(
-                                PopupMenuButton(
-                                  tooltip: 'Alert fields',
-                                  itemBuilder: (context) =>
-                                      enabledFields(e.ignore)
-                                          .map(
-                                            (e) => PopupMenuItem(
-                                              child: Text(e.name.titleCase),
-                                            ),
-                                          )
-                                          .toList(),
-                                  child: AlertFieldsButton(
-                                    ignoredFields: e.ignore,
+                            ),
+                          )
+                          .toList(),
+                      child: FastBuilder(
+                        () => AlertFieldsButton(
+                          ignoredFields: ignoredFields..register(),
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                      tooltip: 'Add alert',
+                      icon: const Icon(Icons.add),
+                      onPressed: addConfig,
+                    ),
+                  ],
+                ),
+              ),
+              if (selectedType.value == AlertConfigType.telegram) ...[
+                const SizedBox(height: 16),
+                const Text('Add the pubstats_bot user to your chat'),
+                const SizedBox(height: 16),
+              ],
+
+              Expanded(
+                child: SingleChildScrollView(
+                  child: FastBuilder(
+                    () => DataTable(
+                      columns: const [
+                        DataColumn(label: Text('Slug')),
+                        DataColumn(label: Text('Type')),
+                        DataColumn(label: Text('Extra')),
+                        DataColumn(label: Text('Fields')),
+                        DataColumn(label: Text('Actions')),
+                      ],
+                      rows: _user.configs
+                          .map(
+                            (e) => DataRow(
+                              cells: [
+                                DataCell(Text(e.slug)),
+                                DataCell(Text(e.type.name.titleCase)),
+                                DataCell(
+                                  SizedBox(width: 200, child: Text(e.extra)),
+                                ),
+                                DataCell(
+                                  PopupMenuButton(
+                                    tooltip: 'Alert fields',
+                                    itemBuilder: (context) =>
+                                        enabledFields(e.ignore)
+                                            .map(
+                                              (e) => PopupMenuItem(
+                                                child: Text(e.name.titleCase),
+                                              ),
+                                            )
+                                            .toList(),
+                                    child: AlertFieldsButton(
+                                      ignoredFields: e.ignore,
+                                    ),
                                   ),
                                 ),
-                              ),
-                              DataCell(
-                                IconButton(
-                                  icon: const Icon(Icons.delete),
-                                  onPressed: () => _user.removeConfig(e),
+                                DataCell(
+                                  IconButton(
+                                    icon: const Icon(Icons.delete),
+                                    onPressed: () => _user.removeConfig(e),
+                                  ),
                                 ),
-                              ),
-                            ],
-                          ),
-                        )
-                        .toList(),
+                              ],
+                            ),
+                          )
+                          .toList(),
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
